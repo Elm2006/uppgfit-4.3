293d61e0a7eeb91634d69a05b15c73f501c8424e
Elm2006 <143172014+Elm2006@users.noreply.github.com>
2023-10-13 13:47:43 +0200

    Add project files.

---
+++ uppgfit 4.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace uppgift_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Detta är en liten text";
+            for(int i = text.Length - 1; i >= 0; i--) 
+            {
+                Console.WriteLine(text[i]);
+            }
+            Console.ReadKey();  
+        }
+    }
+}
